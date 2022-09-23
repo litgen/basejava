d17e8c5185a693d028b1f15038df329c9a87078c
Garik <garikdatsky@gmail.com>
2022-09-23 02:06:06 +0200

    pre hw13 create config and init db

---
+++ src/ru/javawebinar/basejava/Config.java
@@ -0,0 +1,30 @@
+package ru.javawebinar.basejava;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Config {
+  protected static final File PROPS = new File("./config/resumes.properties");
+  private static final Config INSTANCE = new Config();
+
+  private final Properties props = new Properties();
+  private final File storageDir;
+
+  public static Config get() {return INSTANCE;}
+
+  public File getStorageDir() {
+    return storageDir;
+  }
+
+  private Config() {
+    try (InputStream is = new FileInputStream(PROPS)) {
+      props.load(is);
+      storageDir = new File(props.getProperty("storage.dir"));
+    } catch (IOException e) {
+      throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+    }
+  }
+}
