df16a965293a48f2b29079af3a5b941a291c7842
Garik <garikdatsky@gmail.com>
2022-11-26 16:21:43 +0100

    hw13

--- src/ru/javawebinar/basejava/Config.java
+++ src/ru/javawebinar/basejava/Config.java
@@ -1,22 +1,27 @@
 package ru.javawebinar.basejava;
 
+import ru.javawebinar.basejava.storage.SqlStorage;
+import ru.javawebinar.basejava.storage.Storage;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.util.Properties;
+import java.io.FileInputStream;
 
 public class Config {
   protected static final File PROPS = new File("./config/resumes.properties");
   private static final Config INSTANCE = new Config();
+  private final Storage storage;
 
   private final File storageDir;
 
   private Config() {
-    final Properties props = new Properties();
-    try (InputStream is = Files.newInputStream(PROPS.toPath())) {
+    try (InputStream is = new FileInputStream(PROPS)) {
+      Properties props = new Properties();
       props.load(is);
       storageDir = new File(props.getProperty("storage.dir"));
+      storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
     } catch (IOException e) {
       throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
     }
@@ -26,5 +31,9 @@ public class Config {
 
   public File getStorageDir() {
     return storageDir;
+  }
+
+  public Storage getStorage() {
+    return storage;
   }
 }
